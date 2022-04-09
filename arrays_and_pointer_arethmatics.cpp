a2b01edf3aa2b4c044823f7629a418995d88621a
Shreyas Mali <83450905+toastsandwich@users.noreply.github.com>
2022-04-09 10:58:34 +0530

    Update arrays_and_pointer_arethmatics.cpp

--- arrays_and_pointer_arethmatics.cpp
+++ arrays_and_pointer_arethmatics.cpp
@@ -2,6 +2,33 @@
 using namespace std;
 int main(){
   
+  int marks[4]={20,19,12,5}
+  cout<<marks[0]<<endl;
+  cout<<marks[1]<<endl;
+  cout<<marks[2]<<endl;
+  cout<<marks[3]<<endl;
   
+  //important point to note that an array always stores garbage value before we assign it a value.
+  
+  /*run this to check it yourselves
+  int array[10];
+  for (int i = 0; i<10 ; i++){
+    cout<<array[i]<<endl;
+  }
+  */
+
+//   now the main part "POINTERS AND ARRAYS" :-                             
+//   int p = marks;         |  //pointer arethmatic:                                 |
+//   cout<<*p++<<endl;      |  // address_new = address_current+(i*size_of_datatype) |
+  int *p = marks;
+  cout<<*(p++);              // do check this out it is important 
+  cout<<*(++p);
+  cout<<*p;
+  
+//   cout<<"The value of marks[0]is "<<*p;
+//   cout<<"The value of marks[1]is "<<*(p+1);
+//   cout<<"The value of marks[2]is "<<*(p+2);
+//   cout<<"The value of marks[3]is "<<*(p+3);
+
   return 0;
 }
