9ba5cd27ba4f52e7c97872b045eef7ce0fe0a4d6
Shreyas Mali <83450905+toastsandwich@users.noreply.github.com>
2022-04-13 02:56:39 +0530

    Create function_and_functionprototype.cpp

---
+++ function_and_functionprototype.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+//function is important part of top down programming.
+
+int sum(int a,int b){
+    return a + d;
+}
+
+int main(){
+ int a;int b;
+ cin>>a>>b;
+  cout<<"operation of 
+return 0; //this means program has sucessfully compiled.
+}
