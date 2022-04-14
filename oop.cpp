ed2d96e88451835e53584dbc465a533cd7859547
Shreyas Mali <83450905+toastsandwich@users.noreply.github.com>
2022-04-14 17:52:46 +0530

    Update oop.cpp

--- oop.cpp
+++ oop.cpp
@@ -1,16 +1,16 @@
-#include<iostream>
-using namespace std;
-class animals{
-	string name;
-	string description;
-	int quantity;
-	int sellprice;
-};
-int main(){
 //  Animals    -->   Cat,Dog
 //  (class)           (obj)
-	 
+ 
 //  main focus on functions not on flow of data
-	cout<<\t\t"Welcome To ThePerfectPiece"<<endl;
-	return 0;
-}
+
+//Object-oriented programming (OOP) is nothing but that which allows the writing of programs with the help of certain classes and real-time objects. We can say that     //this approach is very close to the real-world and its applications because the state and behaviour of these classes and objects are almost the same as real-world       //objects.
+
+ /*What Are Class & Object? 
+It is the basic concept of OOP; an extended concept of the structure used in C. It is an abstract and user-defined data type. It consists of several variables and       functions. The primary purpose of the class is to store data and information.*/
+//IMPORTANT The class is not visible to the world, but the object is.
+//An basic example for OOPs
+#include<iostream>
+
+	
+	
+
