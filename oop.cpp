3dfc2fac6d8d7b4cdb57fbcd3a792a23be1fd64b
Shreyas Mali <83450905+toastsandwich@users.noreply.github.com>
2022-04-14 14:17:05 +0530

    Create oop.cpp

---
+++ oop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class animals{
+	string name;
+	string description;
+	int quantity;
+	int sellprice;
+};
+int main(){
+//  Animals    -->   Cat,Dog
+//  (class)           (obj)
+	 
+//  main focus on functions not on flow of data
+	cout<<\t\t"Welcome To ThePerfectPiece"<<endl;
+	return 0;
+}
