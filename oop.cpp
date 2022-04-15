a73ff14ad8b8cfb12aaa3e6199442e3b9a86e2ba
Shreyas Mali <83450905+toastsandwich@users.noreply.github.com>
2022-04-15 14:45:55 +0530

    Update oop.cpp

--- oop.cpp
+++ oop.cpp
@@ -10,6 +10,42 @@ It is the basic concept of OOP; an extended concept of the structure used in C. 
 //IMPORTANT The class is not visible to the world, but the object is.
 //An basic example for OOPs
 #include<iostream>
+using namespace std;
+
+class cars;
+    public :
+        int distance_travelled;
+        string engine_number,model_number,chassis_number,model_name,description,damage,color[4],fuelpump_number;
+        void display(){
+            
+        };
+int main (){
+    car car1;
+	cout<<"MODEL NAME : ";
+    cin>>car1.model_name;
+    cout<<"MODEL NUMBER : ";
+    cin>>car1.model_number;
+    cout<<"ENGINE NUMBER";
+    cin>>car1.engine_number;
+    cout<<"CHASSIS NUMBER : ";
+    cin>>car1.chassis_number;
+    cout<<"FUELPUMP NUMBER";
+    cin>>car1.fuelpump_number;
+    for(int i = 0 ; i < 4 ; i++){
+        cout<<"COLOR "<<i+1<<" : ";
+        cin<<car1.color[i];
+    }
+    cout<<"DESCRIPTION : ";
+    cin>>car1.description;
+    cout<<"DAMAGE : ";
+    cin>>car1.damage;
+    cout<<"DISTANCE TRAVELLED :";
+    cin>>car1.distance_travelled;
+    
+    
+    
+    
+}
 
 	
 	
